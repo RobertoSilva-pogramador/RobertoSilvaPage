8b315d356d9bcdaf08503b4f99ebbd2cd3d2ed94
Roberto Silva <robertosilvapessoal@gmail.com>
2025-07-22 18:49:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
     const menu = document.querySelector('.navegacao .menu');
     menu.classList.toggle('ativo');
   }
+function toggleMenu() {
+  document.querySelector('.menu').classList.toggle('ativo');
+}
+
+// DARK MODE
+const darkToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+// Verifica se já foi salvo no localStorage
+if (localStorage.getItem('modoEscuro') === 'ativado') {
+  body.classList.add('dark-mode');
+}
+
+darkToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('modoEscuro', 'ativado');
+  } else {
+    localStorage.setItem('modoEscuro', 'desativado');
+  }
+});
